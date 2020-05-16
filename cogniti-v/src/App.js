b117f0ecdc4f2db59b8cd28f30b4f56e89619bdf
Andrei <37360070+ahuiban@users.noreply.github.com>
2020-05-16 11:43:38 -0400

    Cogniti-V

--- cogniti-v/src/App.js
+++ cogniti-v/src/App.js
@@ -1,9 +1,14 @@
-import React from "react";
-
+import React, { Component } from "react";
+import Navigation from "./components/Navigation/Navigation";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+      </div>
+    );
+  }
 }
-
 export default App;
