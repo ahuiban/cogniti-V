a66460bb78499e6aa872245ff982cf0bc7689475
Andrei <37360070+ahuiban@users.noreply.github.com>
2020-05-19 20:22:30 -0400

    Cogniti-V

--- cogniti-v/src/App.js
+++ cogniti-v/src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import Navigation from "./components/Navigation/Navigation";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import "./App.css";
+
+const app = new Clarifai.App({
+  apiKey: "1b83609b0b0346a382aa627bdc176c64",
+});
 
 const particlesOptions = {
   particles: {
@@ -19,6 +25,35 @@ const particlesOptions = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSumbit = () => {
+    console.log("click");
+
+    app.models
+      .predict(
+        "e466caa0619f444ab97497640cefc4dc",
+        "https://samples.clarifai.com/celebrity.jpeg"
+      )
+      .then(
+        function (response) {
+          console.log(response);
+        },
+        function (err) {
+          // there was an error
+        }
+      );
+  };
+
   render() {
     return (
       <div className="App">
@@ -27,8 +62,12 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSumbit}
+        />
+
+        <FaceRecognition />
       </div>
     );
   }
