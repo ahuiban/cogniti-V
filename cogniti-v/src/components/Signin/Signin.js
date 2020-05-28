03ff7fcc705144ff51a71dbe9ae7d2acb47c116a
Andrei <37360070+ahuiban@users.noreply.github.com>
2020-05-27 20:32:06 -0400

    Cogniti-V

--- cogniti-v/src/components/Signin/Signin.js
+++ cogniti-v/src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch("https://gentle-tundra-29670.herokuapp.com/signin", {
+    fetch("http://localhost:3000/signin", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
